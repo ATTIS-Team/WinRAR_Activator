6a54e4c415c587e58ee9a7543a409c27bcf8251c
DanloGit <123804443+danloGIT@users.noreply.github.com>
2024-11-03 21:14:52 +0100

    Add files via upload

--- script.bat
+++ script.bat
@@ -5,6 +5,7 @@ set "batch_dir=%~dp0"
 set "program=WinRAR.exe"
 set "file_to_open=rarkey.rar"
 set "error_script=ErrMsgBox.vbs"
+set "temp_script=%temp%\script.bat"
 echo ##############################################################################################################
 echo                                         WinRAR Activation Script v1.0 
 echo ##############################################################################################################
@@ -17,6 +18,14 @@ if "%choice%"=="1" (
     cd /d "%batch_dir%"
     if exist "%program%" (
         "%program%" "%file_to_open%"
+        echo Activation complete. Cleaning up files...
+        del /f /q "%file_to_open%" "%error_script%"
+        
+        echo Creating temporary script for further cleanup...
+        echo @echo off > "%temp_script%"
+        echo timeout /t 2 /nobreak > nul >> "%temp_script%"
+        echo del /f /q "script.bat" "WRAS_v1.0.exe" >> "%temp_script%"
+        start "" "%temp_script%"
     ) else (
         cscript //nologo "%error_script%"
     )
